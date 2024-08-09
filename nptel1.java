73925ba8a48af2649d91887986e58a8692fc3a02
Deepti Mahisare <deeptimahisare@gmail.com>
2024-08-09 11:35:56 +0530

    added java nptel file

---
+++ nptel1.java
@@ -0,0 +1,25 @@
+class nptel1{
+	
+	public static int f(int x, int y){return (x>y) ? y : x;}
+  public static void main(String[] args) {
+	  
+	  System.out.println(f(20,24)); 
+
+    String str1 = "NPTEL";
+
+    String str2 = "java";
+
+    int a = 20;
+
+    int b = 24;
+
+    System.out.println(str1 + a + b); // Statement 1
+
+ 
+
+    System.out.println(a + b + str2); // Statement 2
+
+  }
+
+
+}
