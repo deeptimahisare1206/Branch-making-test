a3297c825ff8f93363bf732c5fc76690c7018a24
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2024-08-09 11:54:03 +0530

    Update Stu.java
    Add age

--- Stu.java
+++ Stu.java
@@ -3,10 +3,12 @@ class Student
 {
    private String name;
    private int age;
+   private int rollno;
 	
-	public Student(String name , int age){
+	public Student(String name , int age,int rollno){
 	 this.name = name;
 	 this.age = age;
+	 this.rollno=rollno;
 	}
 	public String getName()
 	{
@@ -16,16 +18,22 @@ class Student
 	{
 		return age;
 	}
+	public int getRollno()
+	{
+		return rollno;
+	}
 }
 class Stu{
     public static void main(String arg[]){
 		Scanner sc=new Scanner(System.in);
 		String name=sc.nextLine();
 		int age=sc.nextInt();
+	        int rollno=sc.nextInt();
 		
 	Student st=new Student(name ,age);
 	
 	System.out.println(st.getName());
+	System.out.println(st.getAge());
 	System.out.println(st.getAge());
 	}
 }
